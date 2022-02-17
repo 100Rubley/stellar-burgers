1204c3e9dcc98468941a758ef9d2311678930309
100Rubley <toxa_98@inbox.ru>
2022-02-17 18:27:09 +0300

    add tests to ingredients reducer

--- react-burger/src/services/redusers/ws-reducer.test.js
+++ react-burger/src/services/redusers/ws-reducer.test.js
@@ -2,7 +2,7 @@ import { wsReducer as reducer } from './ws-reducer'
 
 describe('ws reducer', () => {
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual([
+    expect(reducer(undefined, {})).toEqual(
       {
         wsConnected: false,
         orders: [],
@@ -11,6 +11,6 @@ describe('ws reducer', () => {
         userOrders: [],
         wsUserConnected: false,
       }
-    ])
+    )
   })
 })
