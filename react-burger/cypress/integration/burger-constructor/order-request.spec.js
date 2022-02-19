2840335d466a9bd9ff37edcc54da4e471168aa56
100Rubley <toxa_98@inbox.ru>
2022-02-19 12:10:25 +0300

    add cy tests gor order request

--- react-burger/cypress/integration/burger-constructor/order-request.spec.js
+++ react-burger/cypress/integration/burger-constructor/order-request.spec.js
@@ -14,11 +14,13 @@ describe('order request ability', function () {
 
       cy.get('@bun').trigger('dragstart')
       cy.get('@dropTarget').trigger('drop')
+      cy.get('@dropTarget').contains('Краторная булка N-200i')
 
       cy.get('[data-cy=ingredientContainer]').contains('Соус Spicy-X').as('sauce')
 
       cy.get('@sauce').trigger('dragstart')
       cy.get('@dropTarget').trigger('drop')
+      cy.get('@dropTarget').contains('Соус Spicy-X')
     }
   )
 
@@ -38,7 +40,42 @@ describe('order request ability', function () {
       cy.get('[data-cy=login_emailInput]').click();
       cy.get('[name=email]').type('test@mail.com');
       cy.get('[name=password]').type('Vovka1337');
-      cy.get('button').contains('Войти').click();
+      cy.get('button')
+        .contains('Войти')
+        .click()
+        .wait(500)
+        .getCookie("accessToken").should('not.be.empty') // <-- проверку на залогинивание делаю через наличие кук
+        .getCookie("refreshToken").should('not.be.empty')// <-- проверку на залогинивание делаю через наличие кук
+        .get("body").should("contain", "Соберите бургер");// <-- после входа редирект на главную
+    }
+  )
+
+  it(
+    'should click order btn and get order number',
+    function () {
+      cy.intercept('POST', 'https://norma.nomoreparties.space/api/orders', { fixture: 'order.json' })
+
+      cy.get('[data-cy=ingredientContainer]').contains('Краторная булка N-200i').as('bun')
+      cy.get('[data-cy=dropTarget]').first().as('dropTarget')
+
+      cy.get('@bun').trigger('dragstart')
+      cy.get('@dropTarget').trigger('drop')
+
+      cy.get('[data-cy=ingredientContainer]').contains('Соус Spicy-X').as('sauce')
+
+      cy.get('@sauce').trigger('dragstart')
+      cy.get('@dropTarget').trigger('drop')
+
+      cy.get('button').contains("Оформить заказ").click().wait(1000)
+      cy.get('[data-cy="orderNumber"]').should('contain', 1)
+    }
+  )
+
+  it(
+    'should close order modal',
+    function () {
+      cy.get('[data-cy=closeModal]').first().click();
+      cy.contains('Идентификатор заказа').should('not.exist');
     }
   )
 }
--- react-burger/src/components/order-details/order-details.tsx
+++ react-burger/src/components/order-details/order-details.tsx
@@ -8,8 +8,8 @@ interface IOrderProps {
 
 const OrderDetails: FC<IOrderProps> = ({ orderId }) => {
   return (
-    <div className={s.wrapper}>
-      <h2 className="text text_type_digits-large"> {orderId === 0 ? null : orderId}  </h2>
+    <div className={s.wrapper} >
+      <h2 className="text text_type_digits-large" data-cy='orderNumber'> {orderId === 0 ? null : orderId}  </h2>
 
       <p className="text text_type_main-medium mt-8 mb-15">Идентификатор заказа</p>
       <img src={orderCheck} alt="check" />
